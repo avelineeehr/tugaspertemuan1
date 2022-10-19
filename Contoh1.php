7014c0c389d7e40e49498c3e93631de4815aa65d
avelineeehr <113746174+avelineeehr@users.noreply.github.com>
2022-10-19 18:14:58 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+	
+	public function index()
+	{
+    // $this->load->view('welcome message');
+
+     echo "<h1>perkenalan</h1>";
+        echo"Nama saya Aveline Bintang Herdanis
+        Saya tingga di daerah Jakarta
+        olah raga yang saya sukai adalah
+        Bulutangkis";
+        
+    }
+}
